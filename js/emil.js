1fd36aa4014c14b10330bdbca3dc8070026a6100
Taxxon <te15.emil.kack@umea.nti.se>
2018-04-17 14:26:09 +0200

    Start
    Har startar projektet genom att fixa allt så att det går att använda.
    Har sen skapat en boll i min canvas i mitten. Den står still. Inga
    problem hitils.

---
+++ js/emil.js
@@ -0,0 +1,96 @@
+var canvas = document.getElementById("modalAnimation");
+canvas.width = 640;
+canvas.height = 480;
+canvas.style.width = canvas.width;
+canvas.style.height = canvas.height;
+
+var ctx = canvas.getContext("2d");
+var frame = 0;
+var status = null;
+var numberOfFrames = 200;
+var slider = document.getElementById("slider");
+slider.max = numberOfFrames;
+
+var stop = document.getElementById("stop");
+var play = document.getElementById("play");
+var pause = document.getElementById("pause");
+var start, ms;
+
+clearCanvas();
+renderTime(frame, 0);
+
+slider.addEventListener("change", function() {
+	frame = slider.value;
+	clearCanvas();
+	renderTime(frame, 0);
+}, true);
+
+stop.addEventListener("click", function() {
+	clearInterval(status);
+	frame = 0;
+	slider.value = frame;
+	status = null;
+	clearCanvas();
+	renderTime(frame, 0);
+	bollGul.reset();
+	play.textContent = "Play";
+}, true);
+
+play.addEventListener("click", function() {
+	if (status === "null") {
+		start = Date.now();
+		status = setInterval(timeline, 33.333333333333336);
+		play.textContent = "Pause";
+	} else {
+		clearInterval(status);
+		status = null;
+		play.textContent = "Play";
+	}
+}, true);
+
+function timeline() {
+
+	slider.value = frame;
+	ms = Date.now() - start;
+	//console.log("Seconds elapsed = " + ms/1000);
+
+	clearCanvas();
+	bollGul.animate(frame);
+	renderTime(frame, ms);
+
+	if (frame == numberOfFrames) {
+		clearInterval(status);
+		status = null;
+	}
+	frame++;
+}
+
+function clearCanvas() {
+	ctx.fillStyle = "rgb(250,250,230)";
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+} 
+
+function renderTime(f, time) {
+	ctx.font = "16px Encode Sans";
+	ctx.fillStyle = "black";
+	ctx.fillText("Frame: " + f + ", Time elapsed: " + time/1000 + "s", 4, 20);
+}
+
+var bollGul = {
+	color: "rgb(222,175,72)",
+	x: 315,
+	y: 235,
+	size: 30,
+	speed: 0,
+	animate: function(f) {
+		ctx.beginPath();
+	    ctx.arc(bollGul.x, bollGul.y, bollGul.size, 0, 2*Math.PI);
+	    ctx.fillStyle = bollGul.color;
+	    ctx.fill();	
+	},
+	reset: function() {
+		boll.x = 0;
+	}
+}
+
+
